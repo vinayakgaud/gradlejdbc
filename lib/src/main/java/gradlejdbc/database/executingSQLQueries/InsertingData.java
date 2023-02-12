c90d8b6ce502999122fab2fbab873ecc5ab9e6dd
Vinayak <vinayak.gaud@gmail.com>
2023-02-12 16:17:12 +0530

    managed images

--- lib/src/main/java/gradlejdbc/database/executingSQLQueries/InsertingData.java
+++ lib/src/main/java/gradlejdbc/database/executingSQLQueries/InsertingData.java
@@ -3,8 +3,6 @@ package gradlejdbc.database.executingSQLQueries;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
