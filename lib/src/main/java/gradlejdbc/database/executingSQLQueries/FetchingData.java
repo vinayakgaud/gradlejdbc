6a75a0984685d8e4dc006a6ece8933299134718e
Vinayak <vinayak.gaud@gmail.com>
2023-02-12 16:01:46 +0530

    uploading complete version of crud application

---
+++ lib/src/main/java/gradlejdbc/database/executingSQLQueries/FetchingData.java
@@ -0,0 +1,47 @@
+package gradlejdbc.database.executingSQLQueries;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.sql.DataSource;
+
+import java.io.FileOutputStream;
+import java.sql.Blob;
+import gradlejdbc.database.createConnection.CreateConnection;
+
+public class FetchingData {
+	public static void main(String[] args) {
+		try {
+			DataSource source = CreateConnection.createDataSource();
+			Connection connection = source.getConnection();
+			try {
+				String sqlString = "Select * from userdata";
+				Statement statement = connection.createStatement();
+				ResultSet rs = statement.executeQuery(sqlString);
+				while (rs.next()) {
+					int id = rs.getInt(1);
+					System.out.println("ID: "+id);
+					String name = rs.getString(2);
+					System.out.println("Name: "+name);
+					Blob blob = rs.getBlob(3);
+					String updateNameString = name.replaceAll(" ", "");
+					byte[] bArr = blob.getBytes(1, (int)blob.length());
+					FileOutputStream fos = new FileOutputStream("G:\\JAVA_Projects\\Full Stack Projects\\gradlejdbc\\gradlejdbc\\lib\\src\\main\\resources\\images\\"+updateNameString+"#"+id+".png");
+					fos.write(bArr);
+					fos.close();
+					System.out.println("Picture: "+updateNameString+"#"+id+".png");
+					System.out.println("Saved the image in images folder under resources,\nPath: G:\\JAVA_Projects\\Full Stack Projects\\gradlejdbc\\gradlejdbc\\lib\\src\\main\\resources\\images\\");
+				}
+				statement.close();
+				rs.close();
+			} finally {
+				// TODO: handle finally clause
+				connection.close();
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.getMessage();
+		}
+	}
+}
--- lib/src/test/java/gradlejdbc/LibraryTest.java
+++ lib/src/test/java/gradlejdbc/LibraryTest.java
@@ -4,11 +4,11 @@
 package gradlejdbc;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LibraryTest {
     @Test void someLibraryMethodReturnsTrue() {
-        Library classUnderTest = new Library();
-        assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should return 'true'");
+        
     }
 }
